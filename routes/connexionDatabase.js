75397949a684634de0fe1f17a23e805cb0d0e29e
Bastien91580 <Bastien91580@yahoo.fr>
2018-05-12 19:21:39 +0200

    sans dec le http

--- routes/connexionDatabase.js
+++ routes/connexionDatabase.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'http://xenowarehouse13.myqnapcloud.com',
+    host     : 'xenowarehouse13.myqnapcloud.com',
     user     : 'root',
     password : 'oM0deithe9ieCheibieJongei0vaiS1c',
     database : 'ps',
