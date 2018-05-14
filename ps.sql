c86561f624aced43d57d2431890c6e88d4607b2e
Bastien91580 <Bastien91580@yahoo.fr>
2018-05-14 22:09:18 +0200

    new script database

--- ps.sql
+++ ps.sql
@@ -1,37 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le :  lun. 14 mai 2018 à 22:08
+-- Version du serveur :  5.6.35
+-- Version de PHP :  7.1.6
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+--
+-- Base de données :  `ps`
+--
 
-CREATE DATABASE IF NOT EXISTS `ps` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `ps`;
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(16) NOT NULL,
-  `pwd` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_uniq` (`email`),
-  UNIQUE KEY `username_uniq` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+--
+-- Structure de la table `bets`
+--
 
-DROP TABLE IF EXISTS `user_friends`;
-CREATE TABLE IF NOT EXISTS `user_friends` (
+CREATE TABLE `bets` (
+  `id` int(11) NOT NULL,
+  `idMatch` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `tokens` int(11) NOT NULL,
+  `choice` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `isPayed` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matchs`
+--
+
+CREATE TABLE `matchs` (
+  `id` int(11) NOT NULL,
+  `idTeam1` int(11) NOT NULL,
+  `idTeam2` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `quotation1` double NOT NULL,
+  `quotation2` double NOT NULL,
+  `quotation3` double NOT NULL,
+  `score` varchar(250) NOT NULL,
+  `result` int(11) NOT NULL,
+  `idSport` int(11) NOT NULL,
+  `idEvent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sports`
+--
+
+CREATE TABLE `sports` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `teams`
+--
+
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `idSport` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `pwd` varchar(32) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `admin` int(11) NOT NULL,
+  `tokens` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `pwd`, `email`, `admin`, `tokens`) VALUES
+(1, 'xenodeux', '3ed7dceaf266cafef032b9d5db224717', 'bastien@gmail.com', 1, 0),
+(2, 'Jean', '8cb4f88ffd80dac9c59859dcea8e2ae4', 'Jean@gmail.com', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_friends`
+--
+
+CREATE TABLE `user_friends` (
   `asking_friend` varchar(16) NOT NULL,
   `asked_friend` varchar(16) NOT NULL,
   `state` tinyint(1) NOT NULL DEFAULT '0',
-  `denied` tinyint(1) NOT NULL DEFAULT '0',
-  UNIQUE KEY `asking_friend` (`asking_friend`,`asked_friend`)
+  `denied` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `bets`
+--
+ALTER TABLE `bets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `matchs`
+--
+ALTER TABLE `matchs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `sports`
+--
+ALTER TABLE `sports`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_friends`
+--
+ALTER TABLE `user_friends`
+  ADD UNIQUE KEY `asking_friend` (`asking_friend`,`asked_friend`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `bets`
+--
+ALTER TABLE `bets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `matchs`
+--
+ALTER TABLE `matchs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `sports`
+--
+ALTER TABLE `sports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
