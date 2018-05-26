23acf6451bc8897f3181d76a7f2ab953a3f61f0d
Bastien91580 <Bastien91580@yahoo.fr>
2018-05-26 23:50:17 +0200

    Change log for test Jenkins

--- routes/connexionDatabase.js
+++ routes/connexionDatabase.js
@@ -1,18 +1,19 @@
 var mysql = require('mysql');
+/*
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'root',
     database : 'ps',
 });
+*/
 
-/*
 var connection = mysql.createConnection({
     host     : 'xenowarehouse13.myqnapcloud.com',
     user     : 'root',
     password : 'oM0deithe9ieCheibieJongei0vaiS1c',
     database : 'ps',
-});*/
+});
 
 connection.connect(function(err) {
     if (err){
