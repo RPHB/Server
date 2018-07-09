fd0b0a74cf32999a02514961725a26eeded24c16
Robin Solde <robin.solde@gfi.fr>
2018-07-09 18:02:07 +0200

    matchlist bdd update in progress

--- routes/match.js
+++ routes/match.js
@@ -64,7 +64,7 @@ router.get('/updateTeamList', function(req, res, next) {
 	var resultJson = "{"
 	function checkTeamExist(value, team) {
 		return new Promise(function(resolve, reject) {
-			
+			team=team.substring(0, team.indexOf("(") - 1);
 			var sql = "insert ignore into teams (id, name) values('"+value+"','" + team + "');";
 			// console.log(team + "       pendans req")
 			con.query(sql, function (err, rows, fields) {
@@ -87,6 +87,43 @@ router.get('/updateTeamList', function(req, res, next) {
 })
 
 router.get('/:team_id', function(req, res, next) {
+	function checkMatchExist(t1,t2,id1, id2,d,score) {
+		return new Promise(function(resolve, reject) {
+			var nomTeam1 = t1
+			var nomTeam2 = t2
+			
+			// console.log("match")
+			// console.log(match)
+			// console.log("nomTeam1")
+			console.log(nomTeam1)
+			// console.log("nomTeam2")
+			console.log(nomTeam2)
+			var date = d.substring(d.indexOf(" ") + 1);
+			date = date.substring(0, date.indexOf(" "));
+			var jour=date.substring(0, 2);
+			var mois=date.substring(3, 5);
+			var année=date.substring(6, 10);
+			date=année+"-"+mois+"-"+jour
+			
+			// var sql = "insert ignore into matchs (idTeam1, idTeam2, date, score, result) values(select id from teams where name='"+nomTeam1+"',select id from teams where name='"+nomTeam2+"','"+date+"','"+score+"', 0);";
+			var sql = "insert ignore into matchs (idTeam1, idTeam2, date, score, result) select teams.id from teams where name='"+nomTeam1+"',select id from teams where name='"+nomTeam2+"','"+date+"','"+score+"', 0);";
+			console.log(sql);
+			// con.query(sql, function (err, rows, fields) {
+				// if (err) return reject(err);
+				// resolve(rows, team, value);
+			// });
+		});
+	}
+	function getTeamId(team1, team2, date, score) {
+		return new Promise(function(resolve, reject) {
+			var sql = "select '"+date+"' as date,'"+score+"' as score,id, name from teams where name='"+team1+"' or name='"+team2+"';";
+			console.log(sql);
+			con.query(sql, function (err, rows, fields) {
+				if (err) return reject(err);
+				resolve(rows);
+			});
+		});
+	}
 	request('http://www.foot-national.com/partage.php?type=3&id='+req.params.team_id).then(function(response){
 		var cpt=0;
 		var body = response["body"];
@@ -112,7 +149,27 @@ router.get('/:team_id', function(req, res, next) {
 						result+='"score":"'+currChild[5].child["0"].child["0"].text+'"';
 						result+="},";
 						++cpt;
+						var match=currChild[4].child["1"].child["0"].text;
+						var nomTeam1 = match.substring(0, match.indexOf("-") - 1);
+						var nomTeam2 = match.substring(match.indexOf("-") + 2);
+						// console.log(
+						getTeamId(nomTeam1, nomTeam2, currChild[2].child["0"].text, currChild[5].child["0"].child["0"].text).then(function(rows, t, v){
+							console.log(rows)
+							// var t1 = rows[0].name;
+							// var t2 = rows[1].name;
+							// var id1 = rows[0].id;
+							// var id2 = rows[1].id;
+							// console.log(t1 + " " + t2 + " " + id1 + " " + id2 + currChild[2].child["0"].text)
+							// if (t2 < t1)
+							// {
+								// t2=rows[0].name;
+								// t1=rows[1].name;
+								// id1 = rows[1].id;
+							    // id2 = rows[0].id;
+							// }
+							//checkMatchExist(t1,t2,id1,id2,currChild[5].child["0"].child["0"].text).then(function(rows, t, v){});
 
+						});
 					}
 				}
 
